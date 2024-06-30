8b3182a0d1e5d4ac54be5b3f0290d0c4f53d1be4
Ritik049 <ritikagrawal047@gmail.com>
2024-06-30 21:21:29 +0530

    Starting the project

---
+++ ThymeleafExplore/src/main/java/com/example/ThymeleafExplore/ThymeleafExploreApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThymeleafExplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafExploreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafExploreApplication.class, args);
+	}
+
+}
---
+++ ThymeleafExplore/src/test/java/com/example/ThymeleafExplore/ThymeleafExploreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThymeleafExplore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafExploreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
